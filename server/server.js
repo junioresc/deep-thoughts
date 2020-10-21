d12ddfeb4e0877e57301b3c7546fed7220d2e6b9
Junioresc <junioresc1092@gmail.com>
2020-10-21 16:16:27 -0400

    added the path module to the server to check if production is true

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const { typeDefs, resolvers } = require(`./schemas`);
 
@@ -18,6 +19,14 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
