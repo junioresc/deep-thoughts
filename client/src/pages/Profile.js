022cc97223ff4cf1f5b12e0dd551f514ebb4e2f6
Junioresc <junioresc1092@gmail.com>
2020-10-25 21:19:22 -0400

    now the user is able to see their dashboard if they log in

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 
 const Profile = props => {
   const { username: userParam } = useParams();
   
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
+
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to='/profile' />;
+  }
 
   if(loading) {
     return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
   }
   
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
 
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -12,7 +12,7 @@ class AuthService {
 
     isTokenExpired(token) {
         try {
-            const decode = decode(token);
+            const decoded = decode(token);
             if (decoded.exp < Date.now() / 1000) {
                 return true;
             } else {
